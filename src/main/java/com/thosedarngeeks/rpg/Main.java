08807b8068c0800be11f006b90b21bcca2cc4e19
Gary Coulbourne <bear@bears.org>
2016-11-17 22:09:56 +0000

    starting to create network proxy

--- src/main/java/com/thosedarngeeks/rpg/Main.java
+++ src/main/java/com/thosedarngeeks/rpg/Main.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import io.netty.bootstrap.ServerBootstrap;
-
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
@@ -21,36 +20,78 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 import java.sql.*;
 
-class Main {
+import org.apache.commons.cli.Options;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
+
+public class Main {
+
+    public Options createOptions() {	
+	final Options options = new Options();
+	
+	options.addOption("d", "directory", true, "Directory to store metadata about the game world in progress");
+	options.addOption("v", "verbose", false, "Be extra verbose in the logging");
+	
+        return options;
+    }
+
+    public void run() throws Exception {
+	EventLoopGroup bossGroup = new NioEventLoopGroup();
+	EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+	try {
+
+	    ServerBootstrap b = new ServerBootstrap();
+	    b.group(bossGroup, workerGroup)
+		.channel(NioServerSocketChannel.class);
+
+	    
+	} finally {
+	    workerGroup.shutdownGracefully();
+	    bossGroup.shutdownGracefully();
+	}
+	       
+	
+    }
+    
     public static void main(String[] args) {
 	Logger logger = LoggerFactory.getLogger(Main.class);
 	logger.info("Starting world...");
 
-	//Collection<Version> nettyVers = Version.identify().values();
-
-	List<String> nettyVers = Version.identify().values().stream()
-	    .map(Version::toString)
-	    .collect(Collectors.toList());
-	
-	StringBuilder vers = new StringBuilder();
-
-	// Get the Netty versions
-	vers.append(String.join(", ", nettyVers));
-
 	// TODO: Get the database versions
 	// "select value from information_schema.settings where name = 'info.VERSION';" 
 
-	// TODO: Get the javascript versions
+	// TODO: Get the javascript versions	
+	NashornScriptEngineFactory nsef = new NashornScriptEngineFactory(); 
+	
 	// TODO: Iterate through the plugins and get those versions
 	
 	logger.info("Java version: "+System.getProperty("java.version"));
 	logger.info("Home Directory: "+System.getProperty("user.home"));
-	
-	logger.info("Software Library Versions: " + vers.toString());
+	logger.info("Scripting Engine: " + nsef.getEngineName() + " - " + nsef.getEngineVersion());
+	logger.info("Scripting Language: " + nsef.getLanguageName() + " - " + nsef.getLanguageVersion());
 
+	List<String> nettyVers = Version.identify().values().stream()
+	  .map(Version::toString)
+	  .collect(Collectors.toList());
+	
+	logger.info("Netty Library Versions: " + String.join(", ", nettyVers));
+
+	try {
+	    new Main().run();
+	} catch (Exception e) {
+	    logger.error("Exception! " + e);
+	}
+	
 	logger.info("Shutting down world...");
     }
 
 
 }
+
+
+
+
+
