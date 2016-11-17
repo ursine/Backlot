cc61fb6f3af34840360704bf7f233197681bcafd
Gary Coulbourne <bear@bears.org>
2016-11-17 19:02:53 +0000

    starting to add logback

--- src/main/java/com/thosedarngeeks/rpg/Main.java
+++ src/main/java/com/thosedarngeeks/rpg/Main.java
@@ -1,8 +1,11 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class Main {
 
     public static void main(String[] args) {
-
+	Logger logger = LoggerFactory.getLogger(Main.class);
+	logger.info("Starting world...");
     }
 
 
