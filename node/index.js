fec4f0ebdf0ec0db2614945c09d915d2bc46f130
Gary Coulbourne <bear@bears.org>
2016-11-30 12:06:04 -0500

    add more fixes for eslint:

--- node/index.js
+++ node/index.js
@@ -1,71 +1,72 @@
 /*
-  Backlot, online game engine
-  (C) 2017  Gary Coulbourne
+ Backlot, online game engine
+ (C) 2017  Gary Coulbourne
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 3 of the License, or
-  (at your option) any later version.
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software Foundation,
-  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
-*/
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
 
-/*jshint esversion: 6 */
+(function () {
+  'use strict';
 
-"use strict";
-
-// Set up local logging
-const winston = require("winston");
-const logger = new(winston.Logger)({
+  // Set up local logging
+  const winston = require('winston');
+  const logger = new (winston.Logger)({
     transports: [
-        new(winston.transports.Console)({
-            "timestamp": true,
-            "colorize": true
-        })
+      new (winston.transports.Console)({
+        'timestamp': true,
+        'colorize': true
+      })
     ]
-});
+  });
 
-// Set up requirements
-const events = require("events");
-const fs = require("fs");
-const path = require("path");
+  // Set up requirements
+  const events = require('events');
+  const fs = require('fs');
+  const path = require('path');
 
-process.chdir(__dirname);
+  process.chdir(__dirname);
 
-logger.info("Starting Backlot server process");
+  logger.info('Starting Backlot server process');
 
-const backlot = {
+  const backlot = {
 
-    init: function() {
-        this.loadI18NStrings();
+    init: function () {
+      this.loadI18NStrings();
 
-        logger.info("Alerting modules that initialization is complete");
-        backlot.emit("init");
+      logger.info('Alerting modules that initialization is complete');
+      backlot.emit('init');
     },
 
-    loadI18NStrings: function() {
+    loadI18NStrings: function () {
       const loadStartTm = process.hrtime();
       const directory = './i18n';
       const i18n = fs.readdirSync(directory);
-      for(let i=0; i < i18n.length; i++) {
+      for (let i = 0; i < i18n.length; i++) {
         const modName = i18n[i];
         const theFile = path.join(__dirname, directory, modName);
         const fileDetails = path.parse(theFile);
 
         // Throw away non-javascript files
-        if (fileDetails.ext != '.js') continue;
+        if (fileDetails.ext !== '.js') {
+          continue;
+        }
 
         const baseName = fileDetails.name;
 
         // Is this being reloaded
-        const reloaded = typeof(global[baseName]) != 'undefined';
+        const reloaded = typeof (global[baseName]) !== 'undefined';
         const loadType = reloaded ? 'Reloaded' : 'Loaded';
 
         if (reloaded) {
@@ -76,34 +77,35 @@ const backlot = {
         const theLangTag = global[baseName].LanguageTag;
 
         const loadEndTm = process.hrtime(loadStartTm);
-        logger.log('info', '%s I18N strings for %s(%s) from %s: %ds %dms', loadType, baseName, theLangTag, theFile, loadEndTm[0], loadEndTm[1]/1000000);
+        logger.log('info', '%s I18N strings for %s(%s) from %s: %ds %dms', loadType, baseName, theLangTag, theFile, loadEndTm[0], loadEndTm[1] / 1000000);
       }
     },
 
-    loadModules: function() {
+    loadModules: function () {
 
     },
 
-    die: function() {
-        logger.warn('Starting to shutdown server');
-        process.exit();
+    die: function () {
+      logger.warn('Starting to shutdown server');
+      process.exit();
     }
 
-};
+  };
 
-backlot.__proto__ = events.EventEmitter.prototype;
+  backlot.__proto__ = events.EventEmitter.prototype;
 
-// Configure the process to handle various signals
-var signals = ['SIGINT', 'SIGABRT', 'SIGSEGV', 'SIGTERM'];
-for (var i = 0; i < signals.length; i++) {
-    var currentSignal = signals[i];
+  // Configure the process to handle various signals
+  const signals = ['SIGINT', 'SIGABRT', 'SIGSEGV', 'SIGTERM'];
+  for (let i = 0; i < signals.length; i++) {
+    const currentSignal = signals[i];
     logger.log('info', 'Adding handler for %s', currentSignal);
-    /*process.on(currentSignal, function() {
-      logger.log('info', 'Received %s', currentSignal);
-      backlot.die();
-      });*/
-}
+    /* process.on(currentSignal, function() {
+     logger.log('info', 'Received %s', currentSignal);
+     backlot.die();
+     }); */
+  }
 
-backlot.init();
+  backlot.init();
 
-logger.info('Exiting Backlot server process');
+  logger.info('Exiting Backlot server process');
+}());
