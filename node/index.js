1b6806e9f38d981ed27dad78ee362544fafebb21
Gary Coulbourne <bear@bears.org>
2016-12-03 11:05:32 -0500

    switched to es5 class types

--- node/index.js
+++ node/index.js
@@ -35,75 +35,72 @@
   const events = require('events');
   const fs = require('fs');
   const path = require('path');
+  const util = require('util');
 
   process.chdir(__dirname);
 
   logger.info('Starting Backlot server process');
 
-  const backlot = {
 
-    init: function () {
-      this.loadI18NStrings();
+  const Backlot = function () {
+    events.EventEmitter.call(this);
+  };
+  util.inherits(Backlot, events.EventEmitter);
 
-      logger.info('Alerting modules that initialization is complete');
-      backlot.emit('init');
-    },
-
-    loadI18NStrings: function () {
-      const loadStartTm = process.hrtime();
-      const directory = './i18n';
-      const i18n = fs.readdirSync(directory);
-      for (let i = 0; i < i18n.length; i++) {
-        const modName = i18n[i];
-        const theFile = path.join(__dirname, directory, modName);
-        const fileDetails = path.parse(theFile);
-
-        // Throw away non-javascript files
-        if (fileDetails.ext !== '.js') {
-          continue;
-        }
-
-        const baseName = fileDetails.name;
-
-        // Is this being reloaded
-        const reloaded = typeof (global[baseName]) !== 'undefined';
-        const loadType = reloaded ? 'Reloaded' : 'Loaded';
-
-        if (reloaded) {
-          delete require.cache[require.resolve(theFile)];
-        }
-        global[baseName] = require(theFile);
-
-        const theLangTag = global[baseName].LanguageTag;
-
-        const loadEndTm = process.hrtime(loadStartTm);
-        logger.log('info', '%s I18N strings for %s(%s) from %s: %ds %dms', loadType, baseName, theLangTag, theFile, loadEndTm[0], loadEndTm[1] / 1000000);
-      }
-    },
-
-    loadModules: function () {
-
-    },
-
-    die: function () {
-      logger.warn('Starting to shutdown server');
-      process.exit();
-    }
-
+  Backlot.prototype.init = function () {
+    this.loadI18NStrings();
+    logger.info('Alerting modules that initialization is complete');
+    backlot.emit('init');
   };
 
-  backlot.__proto__ = events.EventEmitter.prototype;
+  Backlot.prototype.loadI18NStrings = function () {
+    const loadStartTm = process.hrtime();
+    const directory = './i18n';
+    const i18n = fs.readdirSync(directory);
+    for (let i = 0; i < i18n.length; i++) {
+      const modName = i18n[i];
+      const theFile = path.join(__dirname, directory, modName);
+      const fileDetails = path.parse(theFile);
+
+      // Throw away non-javascript files
+      if (fileDetails.ext !== '.js') {
+        continue;
+      }
+
+      const baseName = fileDetails.name;
+
+      // Is this being reloaded
+      const reloaded = typeof (global[baseName]) !== 'undefined';
+      const loadType = reloaded ? 'Reloaded' : 'Loaded';
+
+      if (reloaded) {
+        delete require.cache[require.resolve(theFile)];
+      }
+      global[baseName] = require(theFile);
+
+      const theLangTag = global[baseName].LanguageTag;
+
+      const loadEndTm = process.hrtime(loadStartTm);
+      logger.log('info', '%s I18N strings for %s(%s) from %s: %ds %dms', loadType, baseName, theLangTag, theFile, loadEndTm[0], loadEndTm[1] / 1000000);
+    }
+  };
+
+  Backlot.prototype.die = function () {
+    logger.warn('Starting to shutdown server');
+    process.exit();
+  };
 
   // Configure the process to handle various signals
   const signals = ['SIGINT', 'SIGABRT', 'SIGSEGV', 'SIGTERM'];
   for (let i = 0; i < signals.length; i++) {
     const currentSignal = signals[i];
     logger.log('info', 'Adding handler for %s', currentSignal);
-    /* process.on(currentSignal, function() {
-     logger.log('info', 'Received %s', currentSignal);
-     backlot.die();
-     }); */
+    //process.on(currentSignal, function() {
+    //logger.log('info', 'Received %s', currentSignal);
+    //backlot.die();
   }
+
+  const backlot = new Backlot();
 
   backlot.init();
 
