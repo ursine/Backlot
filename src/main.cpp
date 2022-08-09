fe1bdda7be02d4a58ddbcbe3bb009b895c21b6ee
Gary Coulbourne <bear@bears.org>
2022-08-08 20:05:00 -0400

    starting

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,27 @@
 
 #include <cstdlib>
 
-int main(int argc, char* argv[]) {
+#include <SDL.h>
+#include <SDL_vulkan.h>
 
-    EXIT_SUCCESS;
+#include <vulkan.hpp>
+
+int main(int argc, char* argv[])
+{
+    VkExtent2D _windowExtent{ 1700 , 900 };
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    auto window_flags = (SDL_WindowFlags)(SDL_WINDOW_VULKAN);
+
+    auto window = SDL_CreateWindow(
+            "Vulkan Engine", //window title
+            SDL_WINDOWPOS_UNDEFINED, //window position x (don't care)
+            SDL_WINDOWPOS_UNDEFINED, //window position y (don't care)
+            _windowExtent.width,  //window width in pixels
+            _windowExtent.height, //window height in pixels
+            window_flags
+    );
+
+    return EXIT_SUCCESS;
 }
