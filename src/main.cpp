d2739269d081965b7ec8fba9f8224eb1549f41ae
Gary Coulbourne <bear@bears.org>
2022-08-10 19:06:34 -0400

    more vulkan

--- src/main.cpp
+++ src/main.cpp
@@ -4,27 +4,87 @@
 
 #include <cstdlib>
 
+#include <vector>
+
 #include <SDL.h>
 #include <SDL_vulkan.h>
+#include <vulkan/vulkan.h>
 
-#include <vulkan/vk_icd.h>
+static VKAPI_ATTR VkBool32 VKAPI_CALL VulkanReportFunc(
+        VkDebugReportFlagsEXT flags,
+        VkDebugReportObjectTypeEXT objType,
+        uint64_t obj,
+        size_t location,
+        int32_t code,
+        const char* layerPrefix,
+        const char* msg,
+        void* userData)
+{
+    printf("VULKAN VALIDATION: %s\n", msg);
+    return VK_FALSE;
+}
 
 int main(int argc, char* argv[])
 {
-    VkExtent2D _windowExtent{ 640 , 400 };
-
     SDL_Init(SDL_INIT_VIDEO);
 
-    auto window_flags = (SDL_WindowFlags)(SDL_WINDOW_VULKAN);
+    const char* window_name = "Vulkan Engine";
 
     auto window = SDL_CreateWindow(
-            "Vulkan Engine", //window title
+            window_name, //window title
             SDL_WINDOWPOS_UNDEFINED, //window position x (don't care)
             SDL_WINDOWPOS_UNDEFINED, //window position y (don't care)
-            _windowExtent.width,  //window width in pixels
-            _windowExtent.height, //window height in pixels
-            window_flags
+            640,  //window width in pixels
+            400, //window height in pixels
+            SDL_WINDOW_VULKAN
     );
+
+
+    unsigned int extensionCount = 0;
+    SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, nullptr);
+    std::vector<const char*> extensionNames(extensionCount);
+    SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, extensionNames.data());
+
+    const std::vector<const char*> validationLayers = {
+            ///has bug
+            //"VK_LAYER_LUNARG_standard_validation"
+    };
+
+    VkApplicationInfo appInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = window_name;
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo instanceCreateInfo = {};
+    instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceCreateInfo.pApplicationInfo = &appInfo;
+    instanceCreateInfo.enabledLayerCount = validationLayers.size();
+    instanceCreateInfo.ppEnabledLayerNames = validationLayers.data();
+    instanceCreateInfo.enabledExtensionCount = extensionNames.size();
+    instanceCreateInfo.ppEnabledExtensionNames = extensionNames.data();
+
+    VkInstance instance;
+    vkCreateInstance(&instanceCreateInfo, nullptr, &instance);
+
+    auto SDL2_vkCreateDebugReportCallbackEXT = (PFN_vkCreateDebugReportCallbackEXT)SDL_Vulkan_GetVkGetInstanceProcAddr();
+
+    VkDebugReportCallbackCreateInfoEXT debugCallbackCreateInfo = {};
+    debugCallbackCreateInfo.sType = VK_STRUCTURE_TYPE_DEBUG_REPORT_CALLBACK_CREATE_INFO_EXT;
+    debugCallbackCreateInfo.flags = VK_DEBUG_REPORT_ERROR_BIT_EXT | VK_DEBUG_REPORT_WARNING_BIT_EXT;
+    debugCallbackCreateInfo.pfnCallback = VulkanReportFunc;
+
+    VkDebugReportCallbackEXT debugCallback;
+    SDL2_vkCreateDebugReportCallbackEXT(instance, &debugCallbackCreateInfo, 0, &debugCallback);
+
+
+
+
+
+
+
 
     SDL_Event e;
     bool bQuit = false;
