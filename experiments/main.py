6f2ba0a52073faedc0a9d97a19a18293afd30aa0
Gary Coulbourne <bear@bears.org>
2022-08-24 21:11:08 -0400

    starting into experimenting with s3

---
+++ experiments/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import boto3
+from botocore import UNSIGNED
+from botocore.config import Config
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
+
+    response = s3.list_objects_v2(
+        Bucket='noaa-nexrad-level2'
+    )
+
+    print(response)
+    
