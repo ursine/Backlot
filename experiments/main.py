ebd7018d54bb3ad0aaaf8100f677d7a91557a03f
Gary Coulbourne <bear@bears.org>
2022-08-26 10:01:20 -0400

    more geolocation stuffs

--- experiments/main.py
+++ experiments/main.py
@@ -3,19 +3,48 @@
 import boto3
 from botocore import UNSIGNED
 from botocore.config import Config
-
+import sqlite3
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    s3 = boto3.client('s3', region_name='us-east-1', config=Config(signature_version=UNSIGNED))
+    print("Creating configuration file with sqlite")
 
-    response = s3.list_objects_v2(
-        Bucket='noaa-nexrad-level2',
-        Delimiter='/',
-        Prefix='2022/01/01/KGLD/'
-    )
+# ----------------------------------------------------------------------------------------------
 
-    print(response)
+# List of stations and locations
+# https://www.ncei.noaa.gov/access/homr/file/nexrad-stations.txt
+# NCDCID   ICAO WBAN  NAME                           COUNTRY              ST COUNTY                         LAT       LON        ELEV   UTC   STNTYPE
+
+# ----------------------------------------------------------------------------------------------
+
+
+# 30 second rate limit on this api
+# https://www.weather.gov/documentation/services-web-api
+# User-Agent: (myweatherapp.com, contact@myweatherapp.com)
+#    GeoJSON: application/geo+json
+#    JSON-LD: application/ld+json
+#    DWML: application/vnd.noaa.dwml+xml
+#    OXML: application/vnd.noaa.obs+xml
+#    CAP: application/cap+xml
+#    ATOM: application/atom+xml
+# Accept: application/cap+xml
+# Feature-Flag: forecast_temperature_qv
+
+# Get all alerts
+# https://api.weather.gov/alerts/active
+
+
+# ----------------------------------------------------------------------------------------------
+
+#    s3 = boto3.client('s3', region_name='us-east-1', config=Config(signature_version=UNSIGNED))
+
+#    response = s3.list_objects_v2(
+#        Bucket='noaa-nexrad-level2',
+#        Delimiter='/',
+#        Prefix='2022/01/01/KGLD/'
+#    )
+
+#    print(response)
 
 
 # https://graphical.weather.gov/xml/
